0f150fe77eb4263ff3244e5ea3803f9284b846df
shashankmarch27 <48916902+shashankmarch27@users.noreply.github.com>
2022-09-23 08:53:04 +0530

    Updated comments

--- lcd_dino_game.ino
+++ lcd_dino_game.ino
@@ -1,5 +1,10 @@
 /*
-  The circuit:
+  Dino game on arduino
+
+  This sketch Demonstrates how to use a 16x2 lCD display, 
+  to recreate the legendary Chrome dino game
+  
+ The circuit:
  * LCD RS pin to digital pin 12
  * LCD Enable pin to digital pin 11
  * LCD D4 pin to digital pin 7
@@ -27,7 +32,7 @@
  This example code is in the public domain.
 */
 
-// include the livrary code:
+// include the library code:
 #include <LiquidCrystal.h>
 
 // defined the pins for the LCD
@@ -38,26 +43,36 @@
 #define D6 5
 #define D7 4
 
-// defined vutton pin for input
+// defined button pin for input
 #define BTN_PIN 2
 
 // defined dinasour position as 2
 #define dinasour_position 2
 
+// variable to check if game is started or stopped
 int game_start = 0;
 
+// variables for the tree position
 int tree0 = 0;
 int tree1 = 0;
 int tree2 = 0;
 int tree3 = 0;
 
+// variable to know the position of treethat has collided
 int collision_tree = 0;
-int dinasour_line = 0;
+
+// variable is set when the dinasour jumps
 int Jump = 0;
+// variable is set after the jump variable to avoid collision
 bool did_jump = false;
+// variable to store the score
 int score = 0;
+// variable to store the score
 int high_score = 0;
+// variable to define the animation of dinasour
 int motion = 0;
+// this variable defines delay in each step in ms
+// reducing this variable increases the game speed
 int game_speed = 300;
 
 // initialize the library by assosicating any needed LCD interface pins
@@ -73,7 +88,7 @@ byte tree[8] = { 0B00100,
                  0B01100,
                  0B00100,
                  0B00100
-               };
+                 };
 
 // defined binaries for dinasour animation
 byte dina1[8] = { 0B00111,
@@ -84,7 +99,7 @@ byte dina1[8] = { 0B00111,
                   0B01100,
                   0B10010,
                   0B10000
-               };
+                  };
 
 byte dina2[8] = { 0B00111,
                   0B00100,
@@ -94,7 +109,7 @@ byte dina2[8] = { 0B00111,
                   0B01100,
                   0B10010,
                   0B00010
-               };
+                  };
 
 // defined a function to spawn a new tree 
 void spawnTree(int &tree, int &tree_previous){
@@ -104,11 +119,13 @@ void spawnTree(int &tree, int &tree_previous){
 
 // defined a function to display the tree
 void displayTree(int &tree){
-  lcd.setCursor(tree , 1);
-  lcd.write((uint8_t)0);
+  if(tree <=16){
+    lcd.setCursor(tree , 1);
+    lcd.write((uint8_t)0); 
+  }
 }
 
-// 
+// defined a function to display the dinasour motion
 void displayDinasour(){
   if(motion){
     lcd.write((uint8_t)1);
@@ -120,14 +137,23 @@ void displayDinasour(){
     }
 }
 
-// function to read button input and initiate jum if game is running if not then start the game
+// defined a function to check if the dinasour collided with a tree
+bool collisionCheck(int &tree){
+  if(tree == dinasour_position){
+    collision_tree = tree;
+  }
+  return (tree == dinasour_position);
+}
+
+// function to read button input and initiate jump if game is running if not then start the game
 void jump(){ 
   if (game_start){
     Jump = 1;
   } 
   else {
     game_start = 1;
-    
+
+    // spawn the 4 trees when the game starts
     tree0 = 15;
     spawnTree(tree1,tree0);
     spawnTree(tree2,tree1);
@@ -135,7 +161,7 @@ void jump(){
   }
 }
 
-
+// Setup
 void setup() {
   // Create the character for the dinasour and tree
   lcd.createChar(0, tree);
@@ -151,8 +177,13 @@ void setup() {
   attachInterrupt(digitalPinToInterrupt(BTN_PIN), jump,  FALLING);
 }
 
+// Super loop
 void loop() {
+
+  // Check if game_start variable is set if yes then start the game
   while (game_start) {
+    
+    //if the tree position is less than 0 then respawn the tree 
     if(tree0 < 0){
       spawnTree(tree0,tree3);
     }
@@ -168,9 +199,14 @@ void loop() {
     if(tree3 < 0){
       spawnTree(tree3,tree2);
     }
+
+    // set did_jump variable to 0 
     did_jump = false;
-    lcd.clear();
     
+    // cleared display to display new frame
+    lcd.clear();
+
+    // if button is pressed, do the jump animation and set did_jump variable to true
     if (Jump){
       lcd.setCursor(dinasour_position , 0);
       Jump = 0;
@@ -180,81 +216,77 @@ void loop() {
       lcd.setCursor(dinasour_position , 1);
     }
 
+    // display the dinasour
     displayDinasour();
 
-    if(tree0 <= 16){
-      displayTree(tree0);
-    }
-
-    if(tree1 <= 16){
-      displayTree(tree1);
-    }
-
-     if(tree2 <= 16){
-      displayTree(tree2);
-    }
+    // if the tree is position is less than 16 then only display it
+    // This is done to avoid avoid false tree appearing on middle display
+    displayTree(tree0);
+    displayTree(tree1);
+    displayTree(tree2);
+    displayTree(tree3);
     
-    if(tree3 <= 16){
-      displayTree(tree3);
-    }
-
-
-    if(((tree0 == dinasour_position) || (tree1 == dinasour_position) || (tree2 == dinasour_position)|| (tree3 == dinasour_position)) && !did_jump )
-    {
-      if(score>high_score){
+    // do a collision check if the dinsour collided with a tree  
+    // collision is detected
+    if((collisionCheck(tree0) || collisionCheck(tree1) ||collisionCheck(tree2) ||collisionCheck(tree3)) && !did_jump ){
+      
+      // if current score is greater than high score then change the high score
+      if(score > high_score){
         high_score=score;
       }
 
-      if(tree0 == dinasour_position){
-        collision_tree = tree0;
-      }
-      else if(tree1 == dinasour_position){
-        collision_tree = tree1;
-      }
-      else if(tree3 == dinasour_position){
-        collision_tree = tree3;
-      }
-      else{
-        collision_tree = tree2;
-      }
-      
-      // collison
-      did_jump = false;
+      // stop the game
       game_start = 0;
+      
+      // collision animation BEGIN
+      // dispaly the dinasour and the tree 
       lcd.clear();
       lcd.setCursor(collision_tree+1,1);
       lcd.write((uint8_t)0);
       lcd.setCursor(collision_tree,1);
       lcd.write((uint8_t)1);
+
+      // wait for some time and clear the display
       delay(300);
       lcd.clear();
       delay(600);
+      
+      // display the dinasour and the tree
       lcd.setCursor(collision_tree+1,1);
       lcd.write((uint8_t)0);
       lcd.setCursor(dinasour_position,1);
       lcd.write((uint8_t)1);
+
+      // wait for some time and clear the display
       delay(600);
       lcd.clear();
       delay(500);
+
+      // display the dinasourr and the tree
       lcd.setCursor(collision_tree+1,1);
       lcd.write((uint8_t)0);
       lcd.setCursor(dinasour_position,1);
       lcd.write((uint8_t)1);
+      // collision animation END
+
+      // clear the display and wait for some time to display high score
       delay(600);
       lcd.clear();
-      lcd.setCursor(0, 0);
       delay(600);
-      lcd.clear();
-      delay(600);
-      lcd.setCursor(0, 0);
+
+      // display the high score
       lcd.print("High Score: ");
       lcd.print(high_score);
       lcd.setCursor(0, 1);
       lcd.print("Score: ");
       lcd.print(score);
+
+      // wait for some time
       delay(1500);
       lcd.clear();
       delay(400);
+
+      // write play again on screen
       lcd.setCursor(0, 0);
       lcd.print("Press Again to");
       lcd.setCursor(0, 1);
@@ -272,6 +304,8 @@ void loop() {
     tree1 --;
     tree2 --;
     tree3 --;
+
+    // delay for time defined by game_speed, before the next frame is rendered
     delay(game_speed);
   }
 }
